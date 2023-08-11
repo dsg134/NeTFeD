94e3fe93cee9bbc359479d560dec035feeb2583a
Daniel Gore <113949169+dsg134@users.noreply.github.com>
2023-08-10 23:45:27 -0400

    Update Thermal_Sense_Buring_Object.cpp

--- Thermal_Sense_Buring_Object.cpp
+++ Thermal_Sense_Buring_Object.cpp
@@ -1,7 +1,3 @@
-// This is the processing code for the AMG8833
-// The largest pixel value from the interpolated pixel array output is considered the hottest point in the image
-// Using a piecewise probability estimation, the probability of a burning object being within frame is estimated
-
 #include <Wire.h>
 #include <Adafruit_AMG88xx.h>
 
@@ -22,6 +18,13 @@ float findMax_y;
 // Probability of detecting fire via thermal imaging
 float thermalFireProbability;
 const float FIRE_THRESHOLD = 32;
+
+// Fire detection module properties
+#define RGB_res_X 1600
+#define RGB_res_Y 1200
+#define IR_res_X 13
+#define IR_res_Y 13
+#define distance 3 //inches
 
 void setup() {
     Serial.begin(9600);
@@ -56,7 +59,6 @@ void populateOutputMatrix() {
             }
         }
     }
-
     maxTemp = store_max;
     if (maxTemp <= FIRE_THRESHOLD) {
       thermalFireProbability = exp(-1 * (maxTemp - FIRE_THRESHOLD) * (maxTemp - FIRE_THRESHOLD)); // Assumme a Gaussian Distribution until FIRE_THRESHOLD is met
@@ -64,6 +66,54 @@ void populateOutputMatrix() {
     else {
       thermalFireProbability = 1;
     }
+}
+
+// Estimate module's distance from the fire using triangulation
+// if this doesn't work, maybe using PID control to change drone position until fire is centered on both sensor matricies before ejection is a good idea
+float estimate_dist_from_fire(double *RGB_params, double *IR_params, double distance) {
+  // the param elements are arrays containing information on the RGB and IR cameras
+
+  // RGB_params
+  double RGB_FOV_x = RGB_params[0]; // horizontal FOV
+  double RGB_FOV_y = RGB_params[1]; // vertical FOV
+  double RGB_W = RGB_params[2]; // width of camera image (pixels)
+  double RGB_H = RGB_params[3]; // height of camera image (pixels)
+  double RGB_fire_center_x = RGB_params[4]; // center of fire x-value (pixels)
+  double RGB_fire_center_y = RGB_params[5]; // center of fire y-value (pixels)
+
+  // IR_params
+  double IR_FOV_x = IR_params[0]; // horizontal FOV
+  double IR_FOV_y = IR_params[1]; // vertical FOV
+  double IR_W = IR_params[2]; // width of camera image (pixels)
+  double IR_H = IR_params[3]; // height of camera image (pixels)
+  double IR_fire_center_x = IR_params[4]; // center of fire x-value (pixels)
+  double IR_fire_center_y = IR_params[5]; // center of fire y-value (pixels)
+
+  // Find RGB angles
+  double RGB_horiz = ((RGB_fire_center_x - RGB_W / 2) / (RGB_W / 2)) * (RGB_FOV_x / 2);
+  double RGB_vert = ((RGB_fire_center_y - RGB_H / 2) / (RGB_H / 2)) * (RGB_FOV_y / 2);
+
+  // Find IR angles
+  double IR_horiz = ((IR_fire_center_x - IR_W / 2) / (IR_W / 2)) * (IR_FOV_x / 2);
+  double IR_vert = ((IR_fire_center_y - IR_H / 2) / (IR_H / 2)) * (IR_FOV_y / 2);
+
+  // Convert to radians
+  RGB_horiz = M_PI * RGB_horiz / 180;
+  RGB_vert = M_PI * RGB_vert / 180;
+  IR_horiz = M_PI * IR_horiz / 180;
+  IR_vert = M_PI * IR_vert / 180;
+
+  // Horizontal distances
+  double x_RGB = distance * tan(RGB_horiz);
+  double x_IR = distance * tan(IR_horiz);
+
+  // Vertical distances
+  double y_RGB = distance * tan(RGB_vert);
+  double y_IR = distance * tan(IR_vert);
+
+  // Estimate distance to fire
+  double dist_to_fire = sqrt(pow((x_RGB - x_IR), 2) + pow((y_RGB - y_IR), 2) + pow(distance, 2));
+  return dist_to_fire;
 }
 
 void loop() {
@@ -96,6 +146,22 @@ void loop() {
     Serial.print(thermalFireProbability);
     Serial.println();
     Serial.println();
+
+    if (Serial.available()) {
+        float receivedValue = Serial.parseFloat();
+        Serial.print("Received Classification Value: ");
+        Serial.println(receivedValue);
+        Serial.print("Combined Probability: ");
+        Serial.println(receivedValue * thermalFireProbability); //maybe do a weighted probability instead???
+
+        //also send the x and y locations for the fire center
+        //this is needed for triangulation --> for findRGB_x and findRGB_y
+        double RGB_params[6] = {68, 50, RGB_res_X, RGB_res_Y, findRGB_x, findRGB_y};
+        double IR_params[6] = {60, 60, IR_res_X, IR_res_Y, findMax_x, findMax_y};
+        double dist_to_fire = estimate_dist_from_fire(RGB_params, IR_params, distance);
+        Serial.print("Distance from Fire: ");
+        Serial.println(dist_to_fire);
+    }
 
     // Delay before the next reading
     delay(1000);
