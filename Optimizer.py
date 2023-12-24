bbc4fe85fc21185cd73af835995f6e1e1e94aa44
Daniel Gore <113949169+dsg134@users.noreply.github.com>
2023-12-24 18:54:43 -0500

    Update Optimizer.py

--- Optimizer.py
+++ Optimizer.py
@@ -5,87 +5,92 @@ import numpy as np
 # Input Parameters
 # ----------------
 
-# IR Sensor Array Information
-IR_angle = 40 # degrees off of back plate
-IR_FOV = 60 # degrees (field of view)
+# Camera 1 Information
+cam_1_angle = 30 # degrees off of back plate (relative to the FOV bisector)
+cam_1_FOV = 68 # degrees (field of view)
 
-# RGB Camera Information
-RGB_angle = 35 # degrees off of back plate
-RGB_FOV = 68 # degrees (field of view)
+# Camera 2 Information
+cam_2_angle = 30 # degrees off of back plate (relative to the FOV bisector)
+cam_2_FOV = 68 # degrees (field of view)
+
+# Thermal Array Information
+thermal_FOV = 60 # degrees (whose bisector is perpendicular to the back plate)
 
 # Distance and Range
 distance = 3 # inches between the IR sensor and RGB camera
-detection_range = 5 # feet away from midpoint of IR sensor and camera centers
+detection_range = 5.5 # feet away from midpoint of IR sensor and camera centers
 
-# NOTE: IR_FOV + IR_angle + RGB_angle MUST be less than 180 degrees, otherwise the sensor FOVs won't overlap.
+# NOTE: cam_1_FOV + cam_1_angle + cam_2_angle MUST be less than 180 degrees, otherwise the sensor FOVs won't overlap.
 
 # Choose Which Variable You Want to Sweep
 # ---------------------------------------
 
 # Variable Sweep Settings
-sweep_variable = 'detection_range' # Choose: IR_angle, RGB_angle, or distance (the other measures are supposed to be known)
+sweep_variable = 'distance' # Choose: cam_1_angle, cam_2_angle, or distance (the other measures are supposed to be known)
 step_size = 0.1
-min_value = 1
-max_value = 10
-# NOTE: If you choose IR_angle or RGB_angle, they will be in degrees, while distance will be in inches, and detection_range will be in feet
+min_value = 0
+max_value = 5
+# NOTE: If you choose cam_1_angle or cam_2_angle, they will be in degrees, while distance will be in inches
 
 # Conversions
-IR_angle = math.radians(IR_angle) # radians
-IR_FOV = math.radians(IR_FOV) # radians
+cam_1_angle = math.radians(cam_1_angle) # radians
+cam_1_FOV = math.radians(cam_1_FOV) # radians
 
-RGB_angle = math.radians(RGB_angle) # radians
-RGB_FOV = math.radians(RGB_FOV) # radians
+cam_2_angle = math.radians(cam_2_angle) # radians
+cam_2_FOV = math.radians(cam_2_FOV) # radians
+
+thermal_FOV = math.radians(thermal_FOV) # radians
 
 distance = distance / 12 # feet
 # detection_range remains the same (in feet)
 
 # Area Calculations
-def estimate_detection_area(IR_angle, IR_FOV, RGB_angle, RGB_FOV, distance, detection_range):
+def estimate_detection_area(cam_1_angle, cam_1_FOV, cam_2_angle, cam_2_FOV, distance, detection_range):
 	# The covered area can be limited to an (approximate) hexagonal figure which can be split into 4 triangles
 	# 4 regions --> regions A, B, C, and D
 
 	# Area of region A
-	opp_angle_A = math.radians(180) - (IR_FOV + IR_angle + RGB_angle)
-	length_A1 = (distance / (2 * math.cos(IR_angle))) * (math.sin(IR_FOV) / math.sin(opp_angle_A))
-	length_A2 = detection_range - ((distance / 2) * math.tan(RGB_angle))
+	opp_angle_A = math.radians(180) - (cam_1_FOV + cam_1_angle + cam_2_angle)
+	length_A1 = (distance / (2 * math.cos(cam_1_angle))) * (math.sin(cam_1_FOV) / math.sin(opp_angle_A))
+	length_A2 = detection_range - ((distance / 2) * math.tan(cam_2_angle))
 
-	area_A = 0.5 * length_A1 * length_A2 * math.sin(math.radians(90) - RGB_angle) # square feet
+	area_A = 0.5 * length_A1 * length_A2 * math.sin(math.radians(90) - cam_2_angle) # square feet
 
 	# Area of region B
-	opp_angle_B = math.radians(180) - (RGB_FOV + IR_angle + RGB_angle)
-	length_B1 = (distance / (2 * math.cos(RGB_angle))) * (math.sin(RGB_FOV) / math.sin(opp_angle_B))
-	length_B2 = detection_range - ((distance / 2) * math.tan(IR_angle))
+	opp_angle_B = math.radians(180) - (cam_2_FOV + cam_1_angle + cam_2_angle)
+	length_B1 = (distance / (2 * math.cos(cam_2_angle))) * (math.sin(cam_2_FOV) / math.sin(opp_angle_B))
+	length_B2 = detection_range - ((distance / 2) * math.tan(cam_1_angle))
 
-	area_B = 0.5 * length_A1 * length_A2 * math.sin(math.radians(90) - IR_angle) # square feet
+	area_B = 0.5 * length_A1 * length_A2 * math.sin(math.radians(90) - cam_1_angle) # square feet
 
 	# Area of region C
-	opp_angle_C = math.radians(180) - (RGB_FOV + IR_angle + RGB_angle)
-	omega_angle_C = math.radians(180) - (RGB_FOV + RGB_angle) - math.asin(math.sin(RGB_FOV + RGB_angle) * (distance / (2 * detection_range)))
-	z_C1 = (detection_range / math.sin(RGB_FOV + RGB_angle)) * math.sin(omega_angle_C);
-	length_C1 = z_C1 - ((distance / (2 * math.cos(RGB_angle))) * (math.sin(IR_angle + RGB_angle) / math.sin(opp_angle_C)))
+	opp_angle_C = math.radians(180) - (cam_2_FOV + cam_1_angle + cam_2_angle)
+	omega_angle_C = math.radians(180) - (cam_2_FOV + cam_2_angle) - math.asin(math.sin(cam_2_FOV + cam_2_angle) * (distance / (2 * detection_range)))
+	z_C1 = (detection_range / math.sin(cam_2_FOV + cam_2_angle)) * math.sin(omega_angle_C);
+	length_C1 = z_C1 - ((distance / (2 * math.cos(cam_2_angle))) * (math.sin(cam_1_angle + cam_2_angle) / math.sin(opp_angle_C)))
 
-	rho_C_1 = (detection_range - ((distance / 2) * math.tan(IR_angle))) ** 2
-	rho_C_2 = ((distance / (2 * math.cos(RGB_angle))) * (math.sin(RGB_FOV) / math.sin(opp_angle_C))) ** 2
-	rho_C_3 = 2 * math.sqrt(rho_C_1) * math.sqrt(rho_C_2) * math.cos(math.radians(90) - IR_angle)
+	rho_C_1 = (detection_range - ((distance / 2) * math.tan(cam_1_angle))) ** 2
+	rho_C_2 = ((distance / (2 * math.cos(cam_2_angle))) * (math.sin(cam_2_FOV) / math.sin(opp_angle_C))) ** 2
+	rho_C_3 = 2 * math.sqrt(rho_C_1) * math.sqrt(rho_C_2) * math.cos(math.radians(90) - cam_1_angle)
 	rho_C = math.sqrt(rho_C_1 + rho_C_2 - rho_C_3)
 
-	gamma_C = math.asin(((detection_range - (0.5 * distance * math.tan(IR_angle))) / rho_C) * math.sin(math.radians(90) - IR_angle))
+	gamma_C = math.asin(((detection_range - (0.5 * distance * math.tan(cam_1_angle))) / rho_C) * math.sin(math.radians(90) - cam_1_angle))
 	zeta_C = math.radians(180) - opp_angle_C - gamma_C
 
 	area_C = 0.5 * rho_C * length_C1 * math.sin(zeta_C)
 
 	# Area of region D
-	opp_angle_D = math.radians(180) - (IR_FOV + IR_angle + RGB_angle)
-	omega_angle_D = math.radians(180) - (IR_FOV + IR_angle) - math.asin(math.sin(IR_FOV + IR_angle) * (distance / (2 * detection_range)))
-	z_D1 = (detection_range / math.sin(IR_FOV + IR_angle)) * math.sin(omega_angle_D);
-	length_D1 = z_D1 - ((distance / (2 * math.cos(IR_angle))) * (math.sin(IR_angle + RGB_angle) / math.sin(opp_angle_D)))
+	opp_angle_D = math.radians(180) - (cam_1_FOV + cam_1_angle + cam_2_angle)
+	omega_angle_D = math.radians(180) - (cam_1_FOV + cam_1_angle) - math.asin(math.sin(cam_1_FOV + cam_1_angle) * (distance / (2 * detection_range)))
+	z_D1 = (detection_range / math.sin(cam_1_FOV + cam_1_angle)) * math.sin(omega_angle_D);
+	length_D1 = z_D1 - ((distance / (2 * math.cos(cam_1_angle))) * (math.sin(cam_1_angle + cam_2_angle) / math.sin(opp_angle_D)))
 
-	rho_D_1 = (detection_range - ((distance / 2) * math.tan(RGB_angle))) ** 2
-	rho_D_2 = ((distance / (2 * math.cos(IR_angle))) * (math.sin(IR_FOV) / math.sin(opp_angle_D))) ** 2
-	rho_D_3 = 2 * math.sqrt(rho_D_1) * math.sqrt(rho_D_2) * math.cos(math.radians(90) - RGB_angle)
+	rho_D_1 = (detection_range - ((distance / 2) * math.tan(cam_2_angle))) ** 2
+	rho_D_2 = ((distance / (2 * math.cos(cam_1_angle))) * (math.sin(cam_1_FOV) / math.sin(opp_angle_D))) ** 2
+	rho_D_3 = 2 * math.sqrt(rho_D_1) * math.sqrt(rho_D_2) * math.cos(math.radians(90) - cam_2_angle)
 	rho_D = math.sqrt(rho_D_1 + rho_D_2 - rho_D_3)
 
-	gamma_D = math.asin(((detection_range - (0.5 * distance * math.tan(RGB_angle))) / rho_D) * math.sin(math.radians(90) - RGB_angle))
+	gamma_D = math.asin(((detection_range - (0.5 * distance * math.tan(cam_2_angle))) / rho_D) * math.sin(math.radians(90) - cam_2_angle))
 	zeta_D = math.radians(180) - opp_angle_D - gamma_D
 
 	area_D = 0.5 * rho_D * length_D1 * math.sin(zeta_D)
@@ -106,21 +111,21 @@ for i in range(1, data_length):
 	x_data[i] = x_data[i - 1] + step_size
 
 # y-axis values
-if sweep_variable == 'IR_angle':
+if sweep_variable == 'cam_1_angle':
 	for j in range(0, data_length):
-		y_data[j] = estimate_detection_area(math.radians(x_data[j]), IR_FOV, RGB_angle, RGB_FOV, distance, detection_range)
-elif sweep_variable == 'RGB_angle':
+		y_data[j] = estimate_detection_area(math.radians(x_data[j]), cam_1_FOV, cam_2_angle, cam_2_FOV, distance, detection_range)
+elif sweep_variable == 'cam_2_angle':
 	for j in range(0, data_length):
-		y_data[j] = estimate_detection_area(IR_angle, IR_FOV, math.radians(x_data[j]), RGB_FOV, distance, detection_range)
+		y_data[j] = estimate_detection_area(cam_1_angle, cam_1_FOV, math.radians(x_data[j]), cam_2_FOV, distance, detection_range)
 elif sweep_variable == 'distance':
 	for j in range(0, data_length):
-		y_data[j] = estimate_detection_area(IR_angle, IR_FOV, RGB_angle, RGB_FOV, x_data[j] / 12, detection_range)
+		y_data[j] = estimate_detection_area(cam_1_angle, cam_1_FOV, cam_2_angle, cam_2_FOV, x_data[j] / 12, detection_range)
 elif sweep_variable == 'detection_range':
 	for j in range(0, data_length):
-		y_data[j] = estimate_detection_area(IR_angle, IR_FOV, RGB_angle, RGB_FOV, distance, x_data[j])
+		y_data[j] = estimate_detection_area(cam_1_angle, cam_1_FOV, cam_2_angle, cam_2_FOV, distance, x_data[j])
 
 # Plot Results
-sweeps = ['IR_angle', 'RGB_angle', 'distance', 'detection_range']
+sweeps = ['cam_1_angle', 'cam_2_angle', 'distance', 'detection_range']
 if (sweep_variable in sweeps) and (max_value > min_value) and ((step_size > 0) and (step_size < max_value)):
 	plt.plot(x_data, y_data)
 	plt.xlabel(sweep_variable)
@@ -129,5 +134,14 @@ if (sweep_variable in sweeps) and (max_value > min_value) and ((step_size > 0) a
 	plt.grid(True)
 	plt.show()
 else:
-	print('No Sweep Variable Detected')
-	print('Area Given Current Input Parameters:', estimate_detection_area(IR_angle, IR_FOV, RGB_angle, RGB_FOV, distance, detection_range), 'Square Feet')
+	print('No sweep variable detected.')
+	print('Area given current input parameters:', estimate_detection_area(cam_1_angle, cam_1_FOV, cam_2_angle, cam_2_FOV, distance, detection_range), 'Square Feet')
+
+	# Estimate area used by thermal array
+	sector_ratio = math.degrees(thermal_FOV) / 360;
+	thermal_area = sector_ratio * math.pi * (detection_range ** 2)
+	print("Thermal Array Area:", thermal_area, "Square Feet")
+
+	# if thermal_area > detection_area --> warn user to re-optimize as usable space is being lost
+	if (thermal_area > estimate_detection_area(cam_1_angle, cam_1_FOV, cam_2_angle, cam_2_FOV, distance, detection_range)):
+		print("Re-optimize, the thermal sensor area is larger than the intersection of the camera areas.")
